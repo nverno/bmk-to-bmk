f02ee33299dcd44129210d274ee577ab9ab93f8b
nverno <noah.v.peart@gmail.com>
2016-07-25 17:10:50 -0400

    default

--- bmk-to-bmk.el
+++ bmk-to-bmk.el
@@ -33,6 +33,17 @@
 (defgroup bmk-to-bmk nil
   "Manage jumping between/bookmarking multiple bookmark files."
   :group 'bookmark)
+
+;; ------------------------------------------------------------
+;;* User Variables
+(defcustom bmk-to-bmk-default-directory
+  (expand-file-name ".emacs.d/etc/data/bookmarks" "~")
+  "Default directory to store bookmark files."
+  :group 'bmk-to-bmk
+  :type 'file)
+
+;; ------------------------------------------------------------
+;;* Internal
 (defvar bookmark-alist)
 (defvar bmk-to-bmk-stack nil)
 
@@ -43,23 +54,53 @@
 
 (defun bmk-to-bmk-handler (bmk-record)
   (push bookmark-default-file bmk-to-bmk-stack)
-  (bookmark-save)
+  (when (> bookmark-alist-modification-count 0)
+    (bookmark-save))
   (setq bookmark-default-file (bookmark-get-filename bmk-record))
   (setq bookmark-alist nil)
   (let (bookmarks-already-loaded)
     (bookmark-maybe-load-default-file))
   (bookmark-bmenu-list))
 
-;; Backtrack
+(defun bmk-to-bmk-make-record (filename)
+  `((filename . ,filename)
+    (handler . bmk-to-bmk-handler)))
+
+;; ------------------------------------------------------------
+;;* User Functions
+
 (defun bmk-to-bmk-back ()
+  "Go back to last bookmark file, saving current if modified."
   (interactive)
   (when (> (length bmk-to-bmk-stack) 0)
-    (bookmark-save)
+    (when (> bookmark-alist-modification-count 0)
+      (bookmark-save))
     (setq bookmark-default-file (pop bmk-to-bmk-stack))
     (setq bookmark-alist nil)
     (let (bookmarks-already-loaded)
       (bookmark-maybe-load-default-file))
     (bookmark-bmenu-list)))
+
+(defun bmk-to-bmk-new (filename &optional current link)
+  "Create new bookmark file, prompting for FILENAME. If `current-prefix-arg' is 
+4 or CURRENT is non-nil, set new bookmark file as current default bookmark file.  
+If `current-prefix-arg' is 16 or LINK is non-nil, create link to new 
+bookmark file from current bookmark menu list."
+  (interactive
+   (list
+    (let ((default-directory (or bmk-to-bmk-default-directory
+                                 default-directory)))
+      (read-file-name "New Bookmark File: "))))
+  (with-temp-buffer
+    (let (bookmark-alist)
+      (bookmark-save nil filename)))
+  (when (or link (equal current-prefix-arg '(16)))
+    (let* ((name (read-from-minibuffer "Bookmark name: "))
+           (record (bmk-to-bmk-make-record filename)))
+      (bookmark-store name record t)))
+  (when (or current (equal current-prefix-arg '(4)))
+    (bmk-to-bmk-handler
+     (bmk-to-bmk-make-record filename))))
 
 ;; ------------------------------------------------------------
 ;;* Minor Mode
@@ -75,7 +116,8 @@ the mode, `toggle' toggles the state.
 
 When bmk-to-bmk mode is enabled, bookmark menus can be both bookmarked
 and jumped between."
-  :keymap '(((kbd "b") . bmk-to-bmk-back))
+  :keymap '(((kbd "b") . bmk-to-bmk-back)
+            ((kbd "n") . bmk-to-bmk-new))
   :lighter "B2B"
   (setq-local bookmark-make-record-function 'bmk-to-bmk-record-function))
 
